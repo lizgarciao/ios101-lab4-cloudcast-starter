fd7c583b17e963cf24f80774cf76c4ede8dd437f
lizgarciao <lizgarova16@gmail.com>
2024-03-14 20:48:15 -0400

    finished lab

--- CloudCast/CurrentWeatherForecast.swift
+++ CloudCast/CurrentWeatherForecast.swift
@@ -8,78 +8,107 @@
 import Foundation
 import UIKit
 
-// Based on https://open-meteo.com/en/docs
-enum WeatherCode: Int {
-  case clearSky = 0
-  case mainlyClear = 1
-  case partlyCloudy = 2
-  case overcast = 3
-  case fog = 45
-  case rimeFog = 48
-  case drizzleLight = 51
-  case drizzleModerate = 53
-  case drizzleDense = 55
-  case freezingDrizzleLight = 56
-  case freezingDrizzleDense = 57
-  case rainSlight = 61
-  case rainModerate = 63
-  case rainHeavy = 65
-  case freezingRainLight = 66
-  case freezingRainHeavy = 67
-  case snowFallSlight = 71
-  case snowFallModerate = 73
-  case snowFallHeavy = 75
-  case snowGrains = 77
-  case rainShowersSlight = 80
-  case rainShowersModerate = 81
-  case rainShowersViolent = 82
-  case snowShowersSlight = 85
-  case snowShowersHeavy = 86
-  case thunderstormSlight = 95
-  case thunderstormSlightHail = 96
-  case thunderstormHeavyHail = 99
-  
-  var description: String {
-    switch self {
-    case .clearSky:
-      return "Clear skies"
-    case .mainlyClear:
-      return "Mainly clear"
-    case .partlyCloudy, .overcast:
-      return "Cloudy or overcast"
-    case .fog, .rimeFog:
-      return "Foggy"
-    case .drizzleLight, .drizzleModerate, .drizzleDense:
-      return "Drizzle"
-    case .freezingDrizzleLight, .freezingDrizzleDense, .freezingRainLight, .freezingRainHeavy:
-      return "Freezing rain"
-    case .rainSlight, .rainModerate, .rainHeavy, .rainShowersSlight, .rainShowersModerate, .rainShowersViolent:
-      return "Rainy"
-    case .snowFallSlight, .snowFallModerate, .snowFallHeavy, .snowGrains, .snowShowersSlight, .snowShowersHeavy:
-      return "Snowy"
-    case .thunderstormSlight, .thunderstormSlightHail, .thunderstormHeavyHail:
-      return "Thunderstorms"
+// Note: CodingKeys is a enumeration that conforms to the 'CodingKey' protocol used to specify the mappig between the property names of Swift structs and the corresponding keys in the JSON data.
+// The mapping is needed becausse the property names in Switft might not always correspond to the keys in the JSON data.
+struct WeatherAPIResponse: Decodable {
+    let currentWeather: CurrentWeatherForecast
+    
+    private enum CodingKeys: String, CodingKey {
+        case currentWeather = "current_weather"
     }
-  }
-  
-  var image: UIImage? {
-    switch self {
-    case .clearSky, .mainlyClear:
-      return UIImage(named: "sun")
-    case .partlyCloudy, .overcast:
-      return UIImage(named: "cloud-sun")
-    case .fog, .rimeFog:
-      return UIImage(named: "fog")
-    case .drizzleLight, .drizzleModerate, .drizzleDense:
-      return UIImage(named: "drizzle")
-    case .freezingDrizzleLight, .freezingDrizzleDense, .freezingRainLight, .freezingRainHeavy:
-      return UIImage(named: "cloud-drizzle")
-    case .rainSlight, .rainModerate, .rainHeavy, .rainShowersSlight, .rainShowersModerate, .rainShowersViolent:
-      return UIImage(named: "cloud-drizzle")
-    case .snowFallSlight, .snowFallModerate, .snowFallHeavy, .snowGrains, .snowShowersSlight, .snowShowersHeavy:
-      return UIImage(named: "snow")
-    case .thunderstormSlight, .thunderstormSlightHail, .thunderstormHeavyHail:
-      return UIImage(named: "lightning")
+}
+
+struct CurrentWeatherForecast: Decodable {
+    let windSpeed: Double
+    let windDirection: Double
+    let temperature: Double
+    let weatherCodeRaw: Int
+    // line below acts as a convenience property that converts the raw weather code (integer) into an instance of the WeatherCode enum, allowing easier access to descriptive weather conditions for display purposes. If the conversion fails, it defaults to .clearSky, indicating clear skies as a fallback.
+    var weatherCode: WeatherCode {
+        return WeatherCode(rawValue: weatherCodeRaw) ?? .clearSky // if initializer fails and we receive nil, default to clearsky
     }
-  }
+    private enum CodingKeys: String, CodingKey {
+        case windSpeed = "windspeed"
+        case windDirection = "winddirection"
+        case temperature = "temperature"
+        case weatherCodeRaw = "weathercode"
+    }
+
+    
+    
+    // Based on https://open-meteo.com/en/docs
+    enum WeatherCode: Int { // the image and description are based on the current WeatherCode obtained from the JSON data
+        case clearSky = 0
+        case mainlyClear = 1
+        case partlyCloudy = 2
+        case overcast = 3
+        case fog = 45
+        case rimeFog = 48
+        case drizzleLight = 51
+        case drizzleModerate = 53
+        case drizzleDense = 55
+        case freezingDrizzleLight = 56
+        case freezingDrizzleDense = 57
+        case rainSlight = 61
+        case rainModerate = 63
+        case rainHeavy = 65
+        case freezingRainLight = 66
+        case freezingRainHeavy = 67
+        case snowFallSlight = 71
+        case snowFallModerate = 73
+        case snowFallHeavy = 75
+        case snowGrains = 77
+        case rainShowersSlight = 80
+        case rainShowersModerate = 81
+        case rainShowersViolent = 82
+        case snowShowersSlight = 85
+        case snowShowersHeavy = 86
+        case thunderstormSlight = 95
+        case thunderstormSlightHail = 96
+        case thunderstormHeavyHail = 99
+        
+        var description: String {
+            switch self {
+            case .clearSky:
+                return "Clear skies"
+            case .mainlyClear:
+                return "Mainly clear"
+            case .partlyCloudy, .overcast:
+                return "Cloudy or overcast"
+            case .fog, .rimeFog:
+                return "Foggy"
+            case .drizzleLight, .drizzleModerate, .drizzleDense:
+                return "Drizzle"
+            case .freezingDrizzleLight, .freezingDrizzleDense, .freezingRainLight, .freezingRainHeavy:
+                return "Freezing rain"
+            case .rainSlight, .rainModerate, .rainHeavy, .rainShowersSlight, .rainShowersModerate, .rainShowersViolent:
+                return "Rainy"
+            case .snowFallSlight, .snowFallModerate, .snowFallHeavy, .snowGrains, .snowShowersSlight, .snowShowersHeavy:
+                return "Snowy"
+            case .thunderstormSlight, .thunderstormSlightHail, .thunderstormHeavyHail:
+                return "Thunderstorms"
+            }
+        }
+        
+        var image: UIImage? {
+            switch self {
+            case .clearSky, .mainlyClear:
+                return UIImage(named: "sun")
+            case .partlyCloudy, .overcast:
+                return UIImage(named: "cloud-sun")
+            case .fog, .rimeFog:
+                return UIImage(named: "fog")
+            case .drizzleLight, .drizzleModerate, .drizzleDense:
+                return UIImage(named: "drizzle")
+            case .freezingDrizzleLight, .freezingDrizzleDense, .freezingRainLight, .freezingRainHeavy:
+                return UIImage(named: "cloud-drizzle")
+            case .rainSlight, .rainModerate, .rainHeavy, .rainShowersSlight, .rainShowersModerate, .rainShowersViolent:
+                return UIImage(named: "cloud-drizzle")
+            case .snowFallSlight, .snowFallModerate, .snowFallHeavy, .snowGrains, .snowShowersSlight, .snowShowersHeavy:
+                return UIImage(named: "snow")
+            case .thunderstormSlight, .thunderstormSlightHail, .thunderstormHeavyHail:
+                return UIImage(named: "lightning")
+            }
+        }
+    }
 }
--- CloudCast/ForecastViewController.swift
+++ CloudCast/ForecastViewController.swift
@@ -7,37 +7,78 @@
 
 import UIKit
 
+struct Location {
+    let name: String
+    let latitude: Double
+    let longitude: Double
+}
+
 class ForecastViewController: UIViewController {
-  
-  @IBOutlet weak var locationLabel: UILabel!
-  @IBOutlet weak var descriptionLabel: UILabel!
-  @IBOutlet weak var temperatureLabel: UILabel!
-  @IBOutlet weak var windspeedLabel: UILabel!
-  @IBOutlet weak var windDirectionLabel: UILabel!
-  @IBOutlet weak var dateLabel: UILabel!
-  @IBOutlet weak var forecastImageView: UIImageView!
-  
-  override func viewDidLoad() {
-    super.viewDidLoad()
-    addGradient()
-  }
-  
-  private func addGradient() {
-    let gradientLayer = CAGradientLayer()
-    gradientLayer.frame = view.bounds
-    gradientLayer.colors = [UIColor(red: 0.54, green: 0.88, blue: 0.99, alpha: 1.00).cgColor,
-                            UIColor(red: 0.51, green: 0.81, blue: 0.97, alpha: 1.00).cgColor]
-    gradientLayer.startPoint = CGPoint(x: 0.5, y: 0)
-    gradientLayer.endPoint = CGPoint(x: 0.5, y: 1)
-    view.layer.insertSublayer(gradientLayer, at: 0)
-  }
-  
-  @IBAction func didTapBackButton(_ sender: UIButton) {
     
-  }
-  
-  @IBAction func didTapForwardButton(_ sender: UIButton) {
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var temperatureLabel: UILabel!
+    @IBOutlet weak var windspeedLabel: UILabel!
+    @IBOutlet weak var windDirectionLabel: UILabel!
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var forecastImageView: UIImageView!
     
-  }
+    private var locations = [Location]() // initialize instance of (Array) class with default values, in this case an empty array of 'Location' objects.
+    private var selectedLocationIndex = 0 // keep track of current selected location
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        addGradient()
+        let sanJose = Location(name: "San Jose", latitude: 37.335480, longitude: -121.893028)
+        let dominicanRepublic = Location(name: "Dominican Republic", latitude: 18.7357, longitude: 70.1627)
+        let italy = Location(name: "Italy", latitude: 41.8719, longitude: 12.5674)
+        locations = [sanJose, dominicanRepublic, italy]
+        
+        changeLocation(withLocationIndex: 0) // show first location when view loads
+    }
+    
+    private func changeLocation(withLocationIndex locationIndex: Int) {
+        guard locationIndex < locations.count else { return } // check to prevent index out of bounds
+        let location = locations[locationIndex]
+        locationLabel.text = location.name // update location title in UI
+        
+        // 'forecast in' is a closure that captures the 'forecast' data returned by the 'festForecast' function.
+        // The 'in' keyword separates the closure parameter 'forecast' and the closure body which accesses and uses the parameter 'forecast'.
+        WeatherForecastService.fetchForecast(latitude: location.latitude, longitude: location.longitude) { forecast in
+            self.configure(with: forecast)
+        }
+    }
+    
+    private func configure(with forecast: CurrentWeatherForecast) {
+        forecastImageView.image = forecast.weatherCode.image
+        descriptionLabel.text = forecast.weatherCode.description
+        temperatureLabel.text = "\(forecast.temperature)"
+        windspeedLabel.text = "\(forecast.windSpeed) mph"
+        windDirectionLabel.text = "\(forecast.windDirection)°"
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "MMMM d, yyyy"
+        dateLabel.text = dateFormatter.string(from: Date())
+    }
+    
+    private func addGradient() {
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.frame = view.bounds
+        gradientLayer.colors = [UIColor(red: 0.54, green: 0.88, blue: 0.99, alpha: 1.00).cgColor,
+                                UIColor(red: 0.51, green: 0.81, blue: 0.97, alpha: 1.00).cgColor]
+        gradientLayer.startPoint = CGPoint(x: 0.5, y: 0)
+        gradientLayer.endPoint = CGPoint(x: 0.5, y: 1)
+        view.layer.insertSublayer(gradientLayer, at: 0)
+    }
+    
+    @IBAction func didTapBackButton(_ sender: UIButton) {
+        selectedLocationIndex = max(0, selectedLocationIndex - 1) // index must be >= 0
+        changeLocation(withLocationIndex: selectedLocationIndex)
+        
+    }
+    
+    @IBAction func didTapForwardButton(_ sender: UIButton) {
+        selectedLocationIndex = min(locations.count - 1, selectedLocationIndex + 1) // index must be <= locations.count - 1
+        changeLocation(withLocationIndex: selectedLocationIndex)
+    }
 }
 
